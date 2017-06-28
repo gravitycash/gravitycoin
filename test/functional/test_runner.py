754d7559f7d3219f1e753e6422f2a0d3d53d0ad9
Suhas Daftuar <sdaftuar@gmail.com>
2017-06-28 14:52:24 -0400

    [qa] Rewrite BIP65 functional tests
    After 122786d0e0170c73536360b705af711e1338adbf, BIP65 activates at
    a particular height (without regard to version numbers of blocks
    below that height).  Rewrite the BIP65 functional tests to take
    this into account, and add a test case that exercises
    OP_CHECKLOCKTIMEVERIFY in a block where the soft-fork is active.
    
    Also moves the bip65 functional test out of the extended test suite.

--- test/functional/test_runner.py
+++ test/functional/test_runner.py
@@ -115,6 +115,7 @@ BASE_SCRIPTS= [
     'listsinceblock.py',
     'p2p-leaktests.py',
     'wallet-encryption.py',
+    'bip65-cltv-p2p.py',
     'uptime.py',
 ]
 
@@ -138,8 +139,6 @@ EXTENDED_SCRIPTS = [
     'rpcbind_test.py',
     # vv Tests less than 30s vv
     'assumevalid.py',
-    'bip65-cltv.py',
-    'bip65-cltv-p2p.py',
     'bipdersig-p2p.py',
     'bipdersig.py',
     'example_test.py',
